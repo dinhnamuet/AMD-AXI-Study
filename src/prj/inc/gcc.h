c061b630a2dc4dc49279f6f922868a580ad86896
dinhnamuet <nam.dinh@intins.vn>
2026-06-01 19:51:18 +0700

    feat: data structures: add doubly linked list
    Add __write_once_size/__read_once_size helpers with volatile-based
    size dispatch to gcc.h, along with WRITE_ONCE/READ_ONCE macros using
    union trick to avoid strict aliasing violations.
    
    Add list.h — doubly linked list with full set of insert, delete, splice,
    rotate, cut, and safe iteration macros.
    
    Signed-off-by: dinhnamuet <nam.dinh@intins.vn>

--- src/prj/inc/gcc.h
+++ src/prj/inc/gcc.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stddef.h>
+
 #ifndef ALWAYS_INLINE
 #define ALWAYS_INLINE inline __attribute__((always_inline))
 #endif
@@ -11,6 +13,49 @@
 #define compiler_barrier() do { \
 	__asm__ __volatile__ ("" ::: "memory"); \
 } while (0)
+
+static ALWAYS_INLINE void __write_once_size(volatile void *p, void *res, int size)
+{
+	switch (size) {
+	case 1: *(volatile unsigned char *)p  = *(unsigned char *)res;  break;
+	case 2: *(volatile unsigned short *)p = *(unsigned short *)res; break;
+	case 4: *(volatile unsigned int *)p   = *(unsigned int *)res;   break;
+	case 8: *(volatile unsigned long long *)p = *(unsigned long long *)res; break;
+	default:
+		compiler_barrier();
+		__builtin_memcpy((void *)p, (const void *)res, size);
+		compiler_barrier();
+	}
+}
+
+static ALWAYS_INLINE void __read_once_size(const volatile void *p, void *res, int size)
+{
+	switch (size) {
+	case 1: *(unsigned char *)res  = *(volatile unsigned char *)p;  break;
+	case 2: *(unsigned short *)res = *(volatile unsigned short *)p; break;
+	case 4: *(unsigned int *)res   = *(volatile unsigned int *)p;   break;
+	case 8: *(unsigned long long *)res = *(volatile unsigned long long *)p; break;
+	default:
+		compiler_barrier();
+		__builtin_memcpy((void *)res, (const void *)p, size);
+		compiler_barrier();
+	}
+}
+
+#define WRITE_ONCE(x, val) \
+({									\
+	union { typeof(x) __val; char __c[1]; } __u =			\
+		{ .__val = (typeof(x))(val) };				\
+	__write_once_size(&(x), __u.__c, sizeof(x));			\
+	__u.__val;							\
+})
+
+#define READ_ONCE(x) \
+({									\
+	union { typeof(x) __val; char __c[1]; } __u;			\
+	__read_once_size(&(x), __u.__c, sizeof(x));			\
+	__u.__val;							\
+})
 
 #ifndef __aligned
 #define __aligned(x)	__attribute__((__aligned__(x)))
